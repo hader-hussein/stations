401d891bd8b3605c08f4ab16b10d804ea8abaeb2
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2024-05-20 14:24:53 +0300

    20/5/2024

--- assets/js/style.js
+++ assets/js/style.js
@@ -156,29 +156,31 @@ $("#file-history").change(function(){
 	readImg(this);
 });
 
-        // Get the modal
-        var modal = document.getElementById("myModal");
-        
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-        
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-          modal.style.display = "block";
-        }
-        
-        // When the user clicks on <span> (x), close the modal
+  //modele//  
+	
+	   function showPopup() {
+		const inputField = document.getElementById("input-field");
+		const popupForm = document.querySelector(".popup-form");
+		const numInputs = parseInt(inputField.value);
+	   
+		for (let i = 0; i < numInputs; i++) {
+		  const input = document.createElement("input");
+		  input.type = "text";
+		  input.name = `input${i}`;
+		  input.placeholder = `ارتفاع الخزان ${i + 1}(بالمتر) `;
+		  popupForm.appendChild(input);
+		}
+	   
+		const popup = document.getElementById("myModal");
+		popup.style.display = "block";
+	   }
+	   
+	   function closePopup() {
+		const popup = document.getElementById("myModal")[0];
+		popup.style.display = "none";
+	   }
+	   var span = document.getElementsByClassName("close")[0];
         span.onclick = function() {
-          modal.style.display = 'none';
+          myModal.style.display = 'none';
         }
-        
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }
-        
+      
